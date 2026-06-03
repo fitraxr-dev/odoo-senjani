774d74681298eef2d1b4eb37a5fc98d30481589b
Yobel El'Roy Doloksaribu <hlfeyexist@proton.me>
2026-06-04 00:50:59 +0700

    feat,experimental: Customise order list and order detail page on ecommerce_web

--- odoo/addons/senjani_website_custom/__manifest__.py
+++ odoo/addons/senjani_website_custom/__manifest__.py
@@ -3,22 +3,26 @@
     'summary': 'Custom frontend untuk katalog dan halaman detail produk Senjani',
     'description': 'Modul spesifik untuk implementasi desain e-commerce Senjani.',
     'author': 'Tim Senjani',
+    'license': 'LGPL-3',
     'category': 'Website/Website',
     'version': '18.0.1.0',
-    'depends': ['website_sale', 'website_sale_stock'],
+    'depends': ['website_sale', 'website_sale_stock', 'stock_delivery'],
     'data': [
         'views/products_item_templates.xml',
         'views/products_templates.xml',
         'views/pdp_templates.xml',
+        'views/order_detail_templates.xml',
+        'views/ir_asset_data.xml',
     ],
     'assets': {
         'web.assets_frontend': [
             'senjani_website_custom/static/src/css/product_item_card.css',
             'senjani_website_custom/static/src/css/filter_drawer.css',
             'senjani_website_custom/static/src/css/product_reviews.css',
+            'senjani_website_custom/static/src/css/order_detail.css',
             'senjani_website_custom/static/src/xml/stock_availability.xml',
         ],
     },
-    'application': False,
+    'application': True,
     'installable': True,
 }
