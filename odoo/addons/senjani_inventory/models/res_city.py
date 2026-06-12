4efbf179fda5303384cdf97d7cd73d572ef3c141
faridaaryani423 <faridaaryani.tif423@polban.ac.id>
2026-06-12 13:57:51 +0700

    Revert "add inventory module"
    This reverts commit 2c0570c2745356c0d4d6dbdf0b91bad4643ce880.

--- odoo/addons/senjani_inventory/models/res_city.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class ResCity(models.Model):
-    _inherit = 'res.city'
-
-    rajaongkir_city_id = fields.Char(string='RajaOngkir City ID', help='ID kota dari RajaOngkir (misal 114 untuk Bandung)')
