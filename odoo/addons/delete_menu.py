ee4392f1304e0e14e8efa535026f622685be092f
Reqi Jumantara Hapid <reqi.jumantara@gmail.com>
2026-06-20 09:54:52 +0700

    feat: add script to remove contact us menu items

--- odoo/addons/delete_menu.py
+++ odoo/addons/delete_menu.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-menus = env['website.menu'].search([('url', '=', '/contactus')])
-for m in menus:
-    print(f"Deleting Menu: {m.name} (id={m.id})")
-    m.unlink()
-env.cr.commit()
-print("Done deleting contact us menus.")
+menus = env['website.menu'].search([('name', 'ilike', 'Contact')])
+print(f"Menus to delete: {menus}")
+if menus:
+    menus.unlink()
+    env.cr.commit()
+    print("Done deleting contact us menus.")
+else:
+    print("No contact us menu found.")
--- odoo/addons/fix_carrier.py
+++ odoo/addons/fix_carrier.py
@@ -1,3 +1,4 @@
+from odoo.addons.senjani_sales_custom.models import sale_advance_payment_inv
 carriers = env['delivery.carrier'].sudo().search([])
 for c in carriers:
     print(f"Carrier: {c.name} | type={c.delivery_type} | fixed_price={c.fixed_price} | published={c.website_published} | active={c.active} | product_id={c.product_id.name}")
